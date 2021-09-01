cdeccf51cd3ad14b53b9d15bc6a176b410111a3d
TeeSkunkki <70936809+TeeSkunkki@users.noreply.github.com>
2021-09-01 16:53:26 +0300

    CameraController toimii

--- Viraruta MÄPS/Assets/Bloods Vs Scrips/CameraController.cs
+++ Viraruta MÄPS/Assets/Bloods Vs Scrips/CameraController.cs
@@ -6,17 +6,17 @@ public class CameraController : MonoBehaviour
 {
 
     public float CameraDistance;
-    private Player Player;
+    private GameObject Player;
 
     // Start is called before the first frame update
     void Start()
     {
-        Player = FindObjectOfType<Player>();
+        Player = GameObject.FindWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-    mainCamera.transform.position = new Vector3(Player.transform.position.x, 10f, Player.transform.position.z);
+    gameObject.transform.position = new Vector3(Player.transform.position.x, CameraDistance, Player.transform.position.z);
     }
 }
