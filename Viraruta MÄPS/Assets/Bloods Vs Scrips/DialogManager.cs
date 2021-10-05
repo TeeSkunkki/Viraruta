5a988407559521e7dcf313f142505ac247847dcb
TrustABilgeRat <TrustABilgeRat@gmail.com>
2021-10-05 15:14:16 +0300

    1map2boys
    updated chapter 1 map

--- Viraruta MÄPS/Assets/Bloods Vs Scrips/DialogManager.cs
+++ Viraruta MÄPS/Assets/Bloods Vs Scrips/DialogManager.cs
@@ -35,7 +35,7 @@ public class DialogManager : MonoBehaviour
         for(int i = 0; i <= DialogAll.Length; i++)
         {
             if(DialogAll[i].ToString() == "<"){
-                Dia
+                
             }
         }
         reader.Close();
